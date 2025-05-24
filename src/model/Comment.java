cff092c3592f07060aba360c69ef59f47bcb6ade
glerique <gael.lerique@gmail.com>
2025-05-24 15:40:17 +0200

    feat(model): add Comment model

---
+++ src/model/Comment.java
@@ -0,0 +1,59 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Comment {
+    private int id;
+    private String content;
+    private User author;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Comment(int id, String content, User author, LocalDateTime createdAt) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+        this.createdAt = createdAt;
+        this.updatedAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
